6780f7cc3e57c3d397403362583b026df03e9a90
hyojeong721 <ase0574@naver.com>
2022-06-16 23:01:44 +0900

    update SWA 14889

---
+++ algorithm/SWA/14889_스타트와링크/s1.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('input.txt')
+
+def dfs(cnt, idx):
+    global res
+    if cnt == N//2:
+        power1 = 0
+        power2 = 0
+        for i in range(N):
+            for j in range(N):
+                if visited[i] and visited[j]:
+                    power1 += arr[i][j]
+                elif not visited[i] and not visited[j]:
+                    power2 += arr[i][j]
+        res = min(abs(power1-power2), res)
+        return
+
+    for i in range(idx, N):
+        if not visited[i]:
+            visited[i] = True
+            dfs(cnt+1, i+1)
+            visited[i] = False
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [False]*(N)
+res = 1e9
+
+dfs(0, 0)
+print(res)
