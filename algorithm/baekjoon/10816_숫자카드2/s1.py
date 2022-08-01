66539bb64a02f8c6a31b0670ea9b79de79b5f113
HyoJeong721 <ase0574@naver.com>
2022-08-01 20:00:42 +0900

    update 0801/baekjoon_숫자카드2

---
+++ algorithm/baekjoon/10816_숫자카드2/s1.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open("input.txt")
+
+N = int(input())
+mine = sorted(list(map(int, input().split())))
+M = int(input())
+get = map(int, input().split())
+res = {}
+
+
+def binary(s, e, num, cnt):
+    if s >= e:
+        return 0
+    m = (s + e) // 2
+    if mine[m] == num:
+        return mine[s:e+1].count(num)
+    elif mine[m] < num:
+        return binary(m + 1, N, num, cnt)
+    elif mine[m] > num:
+        return binary(s, m - 1, num, cnt)
+
+res = {}
+for m in mine:
+    s, e = 0, N
+    if m not in res:
+        res[m] = binary(s, e, m, 0)
+
+for x in get:
+    if x in res:
+        print(res[x], end = ' ')
+    else:
+        print(0, end = ' ')
