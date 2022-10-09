bc0612a68825f76bc80f7518bc70bbb7c5664984
Hyojeong721 <ase0574@naver.com>
2022-10-09 20:41:32 +0900

    update 15651_N과M(3)

---
+++ algorithm/baekjoon/15651_N과M(3)/221009.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('input.txt')
+
+n, m = map(int, input().split())
+
+s = []
+
+
+def dfs():
+    if len(s) == m:
+        print(*s)
+        return
+
+    for i in range(1, n + 1):
+        s.append(i)
+        dfs()
+        s.pop()
+
+
+dfs()
