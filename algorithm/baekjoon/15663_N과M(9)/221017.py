52f9b8ddb3ef8682856618c89a1b579ae0c8ba41
HyoJeong721 <ase0574@naver.com>
2022-10-17 18:46:14 +0900

    update N과 M 복습

---
+++ algorithm/baekjoon/15663_N과M(9)/221017.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+res = []
+visited = [False]*n
+
+def dfs():
+    global ans
+    if len(res) == m:
+        print(' '.join(map(str, res)))
+        return
+
+    overlap = 0
+    for i in range(n):
+        if not visited[i] and overlap != arr[i]:
+            res.append(arr[i])
+            visited[i] = True
+            overlap = arr[i]
+            dfs()
+            visited[i] = False
+            res.pop()
+
+dfs()
+
+
+
