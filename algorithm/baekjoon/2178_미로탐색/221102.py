06ce5d1bc762b17158c9a56014ae335119172510
HyoJeong721 <ase0574@naver.com>
2022-11-03 11:18:07 +0900

    update 2178 미로탐색1/2206 벽부수고이동하기

--- algorithm/baekjoon/2178_미로탐색/221102.py
+++ algorithm/baekjoon/2178_미로탐색/221102.py
@@ -8,19 +8,30 @@ arr = [list(map(int, input())) for _ in range(N)]
 res = -1
 di, dj = [-1, 1, 0, 0], [0, 0, -1, 1]
 que = deque()
-que.append((0, 0))
+# visitied = [[False]*M for _ in range(N)]
+
+
+que.append((1, 0, 0))
+# visitied[0][0] = True
+
 
 def isrange(i, j):
-    if 0<=i<N and j <= j < M:
+    if 0 <= i < N and 0 <= j < M:
         return True
     return False
 
 while que:
-    i, j = que.popleft()
+    # print(que)
+    # for k in range(N):
+    #     print(*arr[k])
+
+    cnt, i, j = que.popleft()
+
     for k in range(4):
         ni, nj = i+di[k], j+dj[k]
         if isrange(ni, nj) and arr[ni][nj] == 1:
-            que.append((ni, nj))
-            arr[ni][nj] = arr[i][j] + 1
+            arr[ni][nj] = max(cnt+1, arr[ni][nj])
+            que.append((cnt+1, ni, nj))
+
 
 print(arr[N-1][M-1])
--- algorithm/baekjoon/2206_벽부수고이동하기/221102.py
+++ algorithm/baekjoon/2206_벽부수고이동하기/221102.py
@@ -1,8 +1,42 @@
 import sys
-sys.stdin = open('input.txt')
+from collections import deque
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
+arr = [list(map(int, input().rstrip())) for _ in range(N)]
 res = -1
 di, dj = [-1, 1, 0, 0], [0, 0, -1, 1]
+visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
 
+que = deque()
+que.append((0, 0, 0))
+visited[0][0][0] = 1
+
+def isrange(i, j):
+    if 0<=i<N and 0<=j<M:
+        return True
+    return False
+
+def dfs():
+    while que:
+        i, j, w = que.popleft()
+        if i == N-1 and j == M-1:
+            return visited[i][j][w]
+
+        for k in range(4):
+            ni, nj = i+di[k], j+dj[k]
+
+            if isrange(ni, nj):
+                # 이동가능한 곳, 방문한적 없는 곳
+                if arr[ni][nj] == 0 and visited[ni][nj][w] == 0:
+                    visited[ni][nj][w] = visited[i][j][w] + 1
+                    que.append((ni, nj, w))
+                # 벽을 안부스고 온 경우, 벽이고
+                elif arr[ni][nj] == 1 and w == 0:
+                    visited[ni][nj][w+1] = visited[i][j][w] + 1
+                    que.append((ni, nj, w+1))
+    return -1
+print(dfs())
+
+# dfs 내부 while문을 함수 안에 안 넣고 그냥 밖에서 돌리면 시간 초과나고
+# 내부에 넣고 돌리면 통과하는 마법
