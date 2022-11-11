59232e57c79e1799ab67b8cc6c149856e89a479f
HyoJeong721 <ase0574@naver.com>
2022-11-11 14:45:29 +0900

    update 13913_숨바꼭질4

--- algorithm/baekjoon/13913_숨바꼭질4/221110.py
+++ algorithm/baekjoon/13913_숨바꼭질4/221110.py
@@ -5,31 +5,26 @@ from collections import deque
 que = deque()
 N, K = map(int, input().split())
 visited = [False]*100001
-visited[N]=True
-que.append(([N], N, 0))
+visited[N] = True
+res_lst = str(N)+','
+que.append((res_lst, N, 0))
 res = 1e9
 
 while que:
     temp = deque()
     root, now, cnt = que.popleft()
-
     if now == K and cnt < res:
         res = cnt
-        res_lst = root[:]
+        res_lst = root
         continue
-
-
 
     temp = [now+1, now-1, now*2]
     for num in temp:
-        root_temp = root[:]
-        if num < 100001 and not visited[num]:
-            if num >= K * 2 or num <= 0:
-                continue
-            else:
-                root_temp.append(num)
-                que.append((root_temp, num, cnt+1))
-                visited[num] = True
+        root_temp = root
+        if 0 <= num <= 100000 and not visited[num]:
+            root_temp = root_temp + str(num) + ','
+            que.append((root_temp, num, cnt+1))
+            visited[num] = True
 
 print(res)
-print(*res_lst)
+print(*res_lst.split(',')[:-1])
